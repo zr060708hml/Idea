83e25c62238014c7ef2d1945fdbf5a34360a36b0
zr060708hml <hml060708@163.com>
2019-10-14 18:02:40 +0800

    和铭利第二次提交

---
+++ cn/xinzhi/pojo/TestGit.java
@@ -0,0 +1,22 @@
+package cn.xinzhi.pojo;
+
+public class TestGit {
+    private String user;
+    private String passWord;
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+}
